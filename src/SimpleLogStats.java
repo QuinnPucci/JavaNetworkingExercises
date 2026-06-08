d0fd379586534f3fba6fb04383aa19545b8fb361
Quinn Pucci <pucciquinn@gmail.com>
2026-06-08 17:44:54 -0600

    finished simple log problem

--- src/SimpleLogStats.java
+++ src/SimpleLogStats.java
@@ -15,11 +15,25 @@ public class SimpleLogStats {
 
 		String fileName = args[0]; // target args
 		String line;
+		int lineCount = 0;
+		int twoHunCount = 0;
+		int totalBytes = 0;
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 			// FILE PROCESSING
 			while ( (line = br.readLine()) != null){
-					System.out.println(line);
+					lineCount++;
+					String[] curLine = line.split(" ");
+					// if index 1 is status code 200
+					if (curLine[1].equals("200")) {
+						twoHunCount++;
+						// up the counter
+					}
+					int curBytes = Integer.parseInt(curLine[2]);
+					totalBytes += curBytes;
 				}
+				System.out.println(lineCount);
+				System.out.println(twoHunCount);
+				System.out.println(totalBytes);
 
 		} 
 		catch (FileNotFoundException e){
